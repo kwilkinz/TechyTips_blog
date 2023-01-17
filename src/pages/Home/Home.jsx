d48d69c5519310e2c2032fa4ae0e215cda83f91d
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-17 13:59:18 -0600

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,15 @@ import EmptyList from "../../components/common/EmptyList/EmptyList";
 import BlogList from "../../components/BlogList/BlogList";
 import Header from "../../components/Header/Header";
 import axios from "axios";
+import { API_BASE_URL } from "../../config/api"
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("https://techytips-backend.onrender.com/api/posts");
+      const res = await axios.get(`${API_BASE_URL}/posts`)
+      // const res = await axios.get("https://techytips-backend.onrender.com/api/posts");
       // const res = await axios.get("api/posts");
       setPosts(res.data);
     };
