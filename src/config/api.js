fb277f6cc9c667de1d39671a7b494d1d67a6b562
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-17 14:05:05 -0600

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,2 @@
-const API_BASE_URL =
+export const API_BASE_URL =
   process.env.RENDER_APP_API_BASE_URL || "http://localhost:5000";
-
-/**
- * Defines the default headers for these functions to work with `json-server`
- */
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
