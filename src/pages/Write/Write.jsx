29fe206a83be54e3bc10becaced79834cc8ce935
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-25 18:15:47 -0600

    fixing: write.jsx /api/posts

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -31,10 +31,11 @@ const Write = () => {
     }
     try {
       // works
-      const res = await axios.post("https://techytips-backend.onrender.com/api/posts/", newPost);
+      const res = await axios.post("https://techytips-backend.onrender.com/api/posts", newPost);
 
       //TODO : Error will post but will not load directly to the post id - 
       window.location.replace("https://techytips-backend.onrender.com/api/posts/" + res.data._id); 
+      // window.location.replace("https://techytips-backend.onrender.com/api/posts")
     } catch (err) {}
   };
 
