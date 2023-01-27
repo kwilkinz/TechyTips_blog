d23741725f34d0aaf224a88f4eefdafb840fc134
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-27 13:16:34 -0600

    singlePost working.. blogItem

--- src/components/BlogItem/BlogItem.jsx
+++ src/components/BlogItem/BlogItem.jsx
@@ -7,13 +7,13 @@ import { useContext } from "react";
 import ReactMarkdown from "react-markdown";
 
 const BlogItem = ({ post }) => {
-  const PF = "https://techytips-backend.onrender.com/images/";
+  const PF = "https://techytips-backend.onrender.com/images";
   const { user } = useContext(Context);
 
   return (
     <div className="blogItem-wrap">
       {post.photo && (
-        <img className="postImg" src={post.photo} alt="" />
+        <img className="postImg" src={PF + post.photo} alt="" />
       )}
       <CategoryTags label={post.category} />
       <Link
@@ -22,12 +22,11 @@ const BlogItem = ({ post }) => {
       >
         <h3>{post.title}</h3>
       </Link>
-      {/* <p className="blogItem-desc">{post.desc}</p> */}
       <ReactMarkdown className="blogItem-desc" children={post.desc} escapeHtml={true} />
 
       <footer>
         <div className="blogItem-author">
-          {/* {user ? <img src={user.profilePic} alt="" /> : <img src="https://img.freepik.com/free-vector/cute-cat-holding-fish-cartoon-icon-illustration-animal-food-icon-concept-isolated-flat-cartoon-style_138676-2171.jpg?w=2000"/> } */}
+          {user ? <img src={user.profilePic} alt="" /> : null }
           
           <div>
             <h6>{post.username}</h6>
